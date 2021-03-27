ed6a896141bd148cc39aa740ec54d9310055819a
KuzmichMichael <kuzmitch.mikh@yandex.by>
2021-03-28 00:07:13 +0300

    solve findIntersectrion problem

---
+++ Main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+const string findIntersection(const string& firstString, const string& secondString);
+const void fillMapWithDigitsFromString(map<int, int>& directMap, const string& directString);
+
+
+int main() {
+
+	string firstInputString{};
+	string secondInputString{};
+
+	cout << "Enter fisrt string: ";
+	getline(cin, firstInputString);
+
+	cout << "Enter second string: ";
+	getline(cin, secondInputString);
+
+	cout << findIntersection(firstInputString, secondInputString);
+
+	return 0;
+}
+
+const string findIntersection(const string& firstString, const string& secondString)
+{
+	string resultString{};
+	
+	map<int, int> intersectionCounter{};
+
+	fillMapWithDigitsFromString(intersectionCounter, firstString);
+	fillMapWithDigitsFromString(intersectionCounter, secondString);
+
+	
+	for (map<int, int>::iterator it = intersectionCounter.begin(); it != intersectionCounter.end(); it++) {
+		if (it->second > 1) {
+			resultString += (to_string(it->first) + " ");
+		}
+	}
+
+	return resultString;
+}
+
+const void fillMapWithDigitsFromString(map<int, int>& directMap, const string& directString)
+{
+	string digitsBuffer{};
+
+	for (size_t i = 0; i < directString.length(); i++) {
+		while (isdigit(directString[i])) {
+			digitsBuffer += directString[i++];
+		}
+
+		if (!digitsBuffer.empty()) {
+			directMap[stoi(digitsBuffer)]++;
+			digitsBuffer = "";
+		}
+	}
+	return void();
+}
+
